62371dd77263984c8c2dfe8f7919a50b29e4d270
Ribel78 <rbelev@abv.bg>
2024-01-29 00:19:59 +0200

    More operator overloads

--- Box - operator overloading/main.cpp
+++ Box - operator overloading/main.cpp
@@ -1,8 +1,12 @@
 
 /*
-- Да се реализира клас Box (кутия) с 3 полета за ширина, дължина и височина, конструктор и сет/гет методи. Да се предефинират оператори които биха позволили следния запис да работи:
+Create Class Box with 3 fields for width, depth and height, a constructor, and set/get methods
+Define operator overloading which allow the following experession to work
 box3 = box1 + box2;
-където box1, box2, box3 са обекти от Box. Box3 се получава когато сложим двете кутии една над друга.
+where box1, box2, box3 are Box objects. 
+Box3 is derived as if we put two boxes on top of each other.
+Add operator overloads for >, < == (for volume comparison), 
+<< to print class data, >> to set new class parameters.
 */
 #include <iostream>
 using namespace std;
@@ -11,22 +15,44 @@ class Box{
     int width = 0;
     int depth = 0;
     int height = 0;
+    // overloaded stream output operator
+    friend ostream &operator<<(ostream& output, Box& b){
+        output << "Box parameters - width:" << b.width << ", depth: " << b.depth << ", height:" << b.height << endl;
+        return output;
+    }
+    // overloaded stream input operator
+    friend istream &operator>>(istream& input, Box& b){
+        input >> b.width >> b.depth >> b.height;
+        return input;
+    }
 public:
-    Box(int w, int d, int h):width(w), depth(d), height(h){};
-    int getWidth(){return width;};
-    int getDepth(){return depth;};
-    int getHeight(){return height;};
+    Box(){};
+    Box(double w, double d, double h):width(w), depth(d), height(h){};
+    double getWidth(){return width;};
+    double getDepth(){return depth;};
+    double getHeight(){return height;};
+    double getVolume() const {return width*depth*height;}
+    // overloaded addition operator (adds heights and picks max widht or depth)
     Box &operator+(Box& box){
         this->width = (this->width > box.width) ? this->width : box.width;
         this->depth = (this->depth > box.depth)? this->depth : box.depth;
         this->height += box.height;
         return *this;
     }
-    friend ostream &operator<<(ostream& out, Box& b){
-        out << "Box parameters - width:" << b.width << ", depth: " << b.depth << ", height:" << b.height << endl;
-        return out;
-    };
+ 
 };
+
+// overloaded comparison operators based on objkect's volume defined outside of class
+bool operator>(const Box& box1, const Box& box2){
+    return box1.getVolume() > box2.getVolume();
+}
+bool operator<(const Box& box1, const Box& box2){
+    return box1.getVolume() < box2.getVolume();
+}
+bool operator==(const Box& box1, const Box& box2){
+    return box1.getVolume() == box2.getVolume();
+}
+
 
 int main(){
     Box Box1(6, 15, 60);
@@ -36,6 +62,14 @@ int main(){
 
     Box Box3 = Box1 + Box2;
     cout << Box3;
+
+    cout << boolalpha;
+    cout << (Box1 < Box2) << endl;
+    cout << (Box1 > Box2) << endl;
+
+    Box Box4;
+    cin >> Box4; // try 40 9 25 for equality comparison (true)
+    cout << (Box3 == Box4) << endl;
 
     return 0;
 }

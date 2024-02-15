fbdb241eaf221b61fc31722af2555d6c1617ad40
Ribel78 <rbelev@abv.bg>
2024-02-15 14:03:04 +0200

    Homework

--- Class_Hierarchy_Homework/Task.h
+++ Class_Hierarchy_Homework/Task.h
@@ -1,13 +1,14 @@
 /*
-Task1
-Напишете програма на С++, която:
-Използва въведени от потребител цели числа
-Проверява дали числото е четно или нечетно
-Ако числото е четно, на екрана се извежда “even”, ако е нечетно – “odd”
-
 Task 2
 Имплементирайте следната йерархия:
 Напишете програма на C++ в която е имплементирана проста йерархия от класове, за да демонстрирате наследяване и полиморфизъм.
 Например: Може да създадете базов клас Shape с метод draw(), и класове Circle and Rectangle , които да “override”-нат метода draw().
 Използвайте указател (pointer) в примера.
+
+Apply the following hierarchy:
+Write a C++ program that implements a simple class hierarchy to demonstrate
+inheritance and polymorphism.
+For example: You can create a base Shape class with a draw() method and
+Circle and Rectangle classes that override the draw() method.
+Use a pointer in the example.
 */

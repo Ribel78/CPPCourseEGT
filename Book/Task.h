f4b4d10b7e59e2a71ca7a409be4f963be0da947f
Ribel78 <rbelev@abv.bg>
2024-02-15 12:12:37 +0200

    added task description

---
+++ Book/Task.h
@@ -0,0 +1,9 @@
+/*
+Create class Book with the following functionality
+
+Enter Book Data
+amount of Books and title, author, year, price, qty
+find a book with minimal price - takes Books vector as argument
+find a book with maximal price - takes Books vector as argument
+find books by author name
+*/

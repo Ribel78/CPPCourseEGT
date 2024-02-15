43268ea01d489cf96fdcffe50f9c8ea0b6c19402
Ribel78 <rbelev@abv.bg>
2024-02-15 12:37:28 +0200

    adding description

---
+++ Functions/Tasks.h
@@ -0,0 +1,19 @@
+/*
+Functions exercises and research
+int sum (int a, int b);
+int getMax (int a, int b, int c);
+
+Recursion resrearch
+//Euclidean algorithm
+int GCD (int a, int b); 
+
+//Euclid's' algorithm
+int eGCD(int a, int b); 
+
+//Number to Binary(as string)
+string toBinary(int number);
+
+// Binary(as string) to Number
+int toDecimal(string sNumber); 
+
+*/

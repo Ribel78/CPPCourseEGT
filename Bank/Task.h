97562df71398f8cac921f26f03edad9d038e4e46
Ribel78 <rbelev@abv.bg>
2024-02-15 12:07:58 +0200

    Included Task description

---
+++ Bank/Task.h
@@ -0,0 +1,8 @@
+/*
+Create Bank class with credit, debit and getBalance functions
+
+Notes for the debit function:
+Withdraw money from the account and ensure that the debit amount does not exceed the account's balance. 
+If it does, the balance should be left unchanged and the function should print a message: "Debit amount exceeded account balance."    
+
+*/
